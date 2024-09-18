3bddd4d7c71be3e2d81af71d1ebaff5132675478
Rodrigo Sandoval <rodrigosandoval4@gmail.com>
2024-09-17 23:34:53 -0600

    Correct!

--- JS/winning.js
+++ JS/winning.js
@@ -1,163 +1,110 @@
-var chart = am4core.create("chartdiv", am4charts.XYChart);
+am5.ready(function() {
 
-/* Add data */
-chart.data = [{
-  "year": "1994",
-  "cars": 1587,
-  "motorcycles": 650,
-  "bicycles": 121
-}, {
-  "year": "1995",
-  "cars": 1567,
-  "motorcycles": 683,
-  "bicycles": 146
-}, {
-  "year": "1996",
-  "cars": 1617,
-  "motorcycles": 691,
-  "bicycles": 138
-}, {
-  "year": "1997",
-  "cars": 1630.0,
-  "motorcycles": 642,
-  "bicycles": 127
-}, {
-  "year": "1998",
-  "cars": 1660,
-  "motorcycles": 699,
-  "bicycles": 105
-}, {
-  "year": "1999",
-  "cars": 1683,
-  "motorcycles": 721,
-  "bicycles": 109
-}, {
-  "year": "2000",
-  "cars": 1691,
-  "motorcycles": 737,
-  "bicycles": 112
-}, {
-  "year": "2001",
-  "cars": 1298,
-  "motorcycles": 680,
-  "bicycles": 101
-}, {
-  "year": "2002",
-  "cars": 1275,
-  "motorcycles": 664,
-  "bicycles": 97
-}, {
-  "year": "2003",
-  "cars": 1246,
-  "motorcycles": 648,
-  "bicycles": 93
-}, {
-  "year": "2004",
-  "cars": 1218,
-  "motorcycles": 637.9,
-  "bicycles": 101
-}, {
-  "year": "2005",
-  "cars": 1213,
-  "motorcycles": 633,
-  "bicycles": 87
-}, {
-  "year": "2006",
-  "cars": 1199,
-  "motorcycles": 621,
-  "bicycles": 79
-}, {
-  "year": "2007",
-  "cars": 1110,
-  "motorcycles": 210,
-  "bicycles": 81
-}, {
-  "year": "2008",
-  "cars": 1165,
-  "motorcycles": 232,
-  "bicycles": 75
-}, {
-  "year": "2009",
-  "cars": 1145,
-  "motorcycles": 219,
-  "bicycles": 88
-}, {
-  "year": "2010",
-  "cars": 1163.8,
-  "motorcycles": 201,
-  "bicycles": 82
-}, {
-  "year": "2011",
-  "cars": 1180,
-  "motorcycles": 285,
-  "bicycles": 87
-}, {
-  "year": "2012",
-  "cars": 1159,
-  "motorcycles": 277,
-  "bicycles": 71
-}];
-
-/* Create axes */
-var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-categoryAxis.dataFields.category = "year";
-
-/* Create value axis */
-var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
- valueAxis.numberFormatter.numberFormat = {
-  "style": "decimal",
-  "minimumFractionDigits": 0
-};  
-
-/* Create series */
-var series1 = chart.series.push(new am4charts.LineSeries());
-series1.dataFields.valueY = "cars";
-series1.dataFields.categoryX = "year";
-series1.name = "Cars";
-series1.strokeWidth = 3;
-series1.tensionX = 0.7;
-series1.bullets.push(new am4charts.CircleBullet());
-series1.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+// Create root element
+// https://www.amcharts.com/docs/v5/getting-started/#Root_element
+var root = am5.Root.new("chartdiv");
 
 
-/* Add legend */
-chart.legend = new am4charts.Legend();
-
-/* Create a cursor */
-chart.cursor = new am4charts.XYCursor();
-
-/* Configure axis tooltip */
-var axisTooltip = categoryAxis.tooltip;
-axisTooltip.background.fill = am4core.color("#07BEB8");
-axisTooltip.background.strokeWidth = 0;
-axisTooltip.background.cornerRadius = 3;
-axisTooltip.background.pointerLength = 0;
-axisTooltip.dy = 5;
-
-var dropShadow = new am4core.DropShadowFilter();
-dropShadow.dy = 1;
-dropShadow.dx = 1;
-dropShadow.opacity = 0.5;
-axisTooltip.filters.push(dropShadow);
+// Set themes
+// https://www.amcharts.com/docs/v5/concepts/themes/
+root.setThemes([
+  am5themes_Animated.new(root)
+]);
 
 
+// Create chart
+// https://www.amcharts.com/docs/v5/charts/xy-chart/
+var chart = root.container.children.push(am5xy.XYChart.new(root, {
+  panX: true,
+  panY: true,
+  wheelX: "panX",
+  wheelY: "zoomX",
+  pinchZoomX:true,
+  paddingLeft: 0
+}));
 
-let exportData = [{
- "year": "2000",
- "car": "BMW"
-  
-},
-{
- "year": "2001",
- "car": "Audi"
-  
-}];
+
+// Add cursor
+// https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
+  behavior: "none"
+}));
+cursor.lineY.set("visible", false);
 
 
-chart.exporting.menu = new am4core.ExportMenu();
+// Generate random data
+var date = new Date();
+date.setHours(0, 0, 0, 0);
+var value = 100;
 
-chart.exporting.adapter.add('data', (data => {
-                    data.data = [];
-                    data.data = exportData;
-                    return data;
-                }));
+function generateData() {
+  value = Math.round((Math.random() * 10 - 5) + value);
+  am5.time.add(date, "day", 1);
+  return {
+    date: date.getTime(),
+    value: value
+  };
+}
+
+function generateDatas(count) {
+  var data = [];
+  for (var i = 0; i < count; ++i) {
+    data.push(generateData());
+  }
+  return data;
+}
+
+
+// Create axes
+// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+var xAxis = chart.xAxes.push(am5xy.DateAxis.new(root, {
+  maxDeviation: 0.2,
+  baseInterval: {
+    timeUnit: "day",
+    count: 1
+  },
+  renderer: am5xy.AxisRendererX.new(root, {
+    minorGridEnabled:true
+  }),
+  tooltip: am5.Tooltip.new(root, {})
+}));
+
+var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+  renderer: am5xy.AxisRendererY.new(root, {
+    pan:"zoom"
+  })  
+}));
+
+
+// Add series
+// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+var series = chart.series.push(am5xy.LineSeries.new(root, {
+  name: "Series",
+  xAxis: xAxis,
+  yAxis: yAxis,
+  valueYField: "value",
+  valueXField: "date",
+  tooltip: am5.Tooltip.new(root, {
+    labelText: "{valueY}"
+  })
+}));
+
+
+// Add scrollbar
+// https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
+chart.set("scrollbarX", am5.Scrollbar.new(root, {
+  orientation: "horizontal"
+}));
+
+
+// Set data
+var data = generateDatas(1200);
+series.data.setAll(data);
+
+
+// Make stuff animate on load
+// https://www.amcharts.com/docs/v5/concepts/animations/
+series.appear(1000);
+chart.appear(1000, 100);
+
+}); // end am5.ready()
