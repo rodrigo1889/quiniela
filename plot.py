28caf33a9150008e531cb2149cb805e1b2178159
Rodrigo Sandoval <rodrigosandoval4@gmail.com>
2024-09-18 21:41:04 -0600

    Including new parameters

--- plot.py
+++ plot.py
@@ -16,10 +16,9 @@ data = data.drop(labels=["Jugador","COM","DIV","FC","SB","TOTAL"],axis=1)
 fig = go.Figure()
 for name in data.index.to_numpy():
 	fig.add_trace(go.Scatter(x = data.T.index,y=data.T[name],name=name,line_shape="spline",
-		line=dict(color=plotter.Color_Picker(name).pick(),width=3),marker=dict(size=10)))
+		line=dict(color=plotter.Color_Picker(name).pick(),width=3),marker=dict(size=9)))
 fig.update_layout(title="Probabilidades de victoria en temporada regular",
 	template="plotly_dark",yaxis_title="% de 10 mil simulaciones ganadas")
 #fig.show()
-#fig.write_html("winning_prob.html",full_html=False)
 print(plotly.offline.plot(fig,filename="winning_prob.html",include_plotlyjs=False,output_type="div"))
 
